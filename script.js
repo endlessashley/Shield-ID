bc63036a9d350803aed64c6f8fa230b59e0ae9bf
endlessashley <81969558+endlessashley@users.noreply.github.com>
2021-07-14 21:27:44 -0400

    script synced

--- script.js
+++ script.js
@@ -1,83 +1,50 @@
 (function () {
-    function giphySearch(keyword) {
-      return fetch(`https://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=XnUlKSQw33KcHiZVIv8ReqDGr7nJZwCS&limit=3`)
-        .then(response => response.json());
-      
-  
-      }
-     function infoSearch(keyword) {
-       return fetch('https://superheroapi.com/api/4431251130239326/search/' + keyword)
-     } 
-
-    function appendImage(img) {
-      let $div = $('<div class="img-wrapper"></div>');
-      $('<div class="inner"></div>').append(img).appendTo($div);
-      $('.gifs').append($div)
-    }
-   
-
-    function onImgLoad(img) {
-      return new Promise((resolve, reject) => {
-        img.onload = resolve;
-      });
-    }
-  
-    (function listenOnFormSubmit() {
-      $('.pure-form').submit(async (ev) => {
-        ev.preventDefault();
-  
-        let $input = $('#searchInput');
-  
-        main($input.val());
-      });
-    })();
-  
-    async function main(keyword) {
-      const result = await giphySearch(keyword);
-      $('.gifs').html('');
-
-      let promises = [];
-      result.data.forEach(gif => {
-        let img = new Image();
-        img.src = gif.images.original.url;
-        promises.push(onImgLoad(img));
-
-        appendImage(img);
-      });
-  
-      await Promise.all(promises);
-    }
+  function giphySearch(keyword) {
+    return fetch(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=XnUlKSQw33KcHiZVIv8ReqDGr7nJZwCS&limit=3`)
+      .then(response => response.json());
+  }
+  function infoSearch(keyword) {
+    return fetch('https://superheroapi.com/api/4431251130239326/search/' + keyword)
+  }
+  function appendImage(img) {
+    let $div = $('<div class="img-wrapper"></div>');
+    $('<div class="inner"></div>').append(img).appendTo($div);
+    $('.gifs').append($div)
+  }
+  function onImgLoad(img) {
+    return new Promise((resolve, reject) => {
+      img.onload = resolve;
+    });
+  }
+  (function listenOnFormSubmit() {
+    $('.pure-form').submit(async (ev) => {
+      ev.preventDefault();
+      let $input = $('#searchInput');
+      main($input.val());
+      loadJSONFile();
+    });
   })();
-
   async function main(keyword) {
     const result = await giphySearch(keyword);
     $('.gifs').html('');
-
     let promises = [];
     result.data.forEach(gif => {
       let img = new Image();
       img.src = gif.images.original.url;
       promises.push(onImgLoad(img));
-
       appendImage(img);
     });
-
     await Promise.all(promises);
   }
-
-
-
+})();
 var heroAPI = "https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/4431251130239326/search/";
-
 // document.getElementById("submitBtn").onclick = function(event) {
 //     event.preventDefault();
 //     loadJSONFile()};
-
 function loadJSONFile() {
   let input = document.getElementById('searchInput').value
   console.log(input)
   console.log("step 1 done.")
-
   var searchURL = heroAPI + input;
   console.log(searchURL);
   fetch(searchURL)
@@ -91,23 +58,15 @@ function loadJSONFile() {
       document.getElementById("hero-name").innerHTML = data.results[0].name;
       //  document.getElementById("full-name").innerHTML = data.results[0].biography.full-name;
       //  document.getElementById("alter-egos").innerHTML = data.results[0].biography.alter-egos;
-
-
       console.log(data.results[0].image.url);
-
       //  document.getElementById("thumbnail").innerHTML.src = data.results[0].image.url;
-
       // let $div = $('<img>');
       $(".thumbnailpic").attr("src",data.results[0].image.url);
       // $div.append(data.results[0].image.url);
       // $('.pure-u-3-5').append($div);
-
-
       console.log(data.results[0].biography);
       document.getElementById("hero-bio").innerHTML = "here is the publisher:" + data.results[0].biography.publisher;
       document.getElementById("job").innerHTML = "Occupation:" + data.results[0].work.occupation;
-
-
       console.log(data.results[0].powerstats);
       document.getElementById("combat").innerHTML = "Combat:" + data.results[0].powerstats.combat;
       document.getElementById("durability").innerHTML = "Durability:  " + data.results[0].powerstats.durability;
@@ -118,7 +77,5 @@ function loadJSONFile() {
       document.getElementById("hero-stats").innerHTML = "here are the speed stats:" + data.results[0].powerstats.speed;
     })
 };
-
 // console.log("step 2 done.");
-
 console.log("step 3 done.");
